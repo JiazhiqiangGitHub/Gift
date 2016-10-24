507409c98ca56699c13aeec0be9265137b446c7f
贾志强 <70686114@qq.com>
2016-10-24 17:50:48 +0800

    完成了各个页面的title 分类页面只布局了一个tablelayout没有具体完善内容待定

--- app/src/main/java/lanou/gift/hot/hotFragment.java
+++ app/src/main/java/lanou/gift/hot/hotFragment.java
@@ -1,9 +1,24 @@
 package lanou.gift.hot;
 
-import android.support.v4.app.Fragment;
+import lanou.gift.R;
+import lanou.gift.base.BaseFragment;
 
 /**
  * Created by dllo on 16/10/21.
  */
-public class hotFragment extends Fragment {
+public class hotFragment extends BaseFragment {
+    @Override
+    protected void initDate() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected int getLayout() {
+        return R.layout.hot_fragment;
+    }
 }
--- app/src/main/java/lanou/gift/sort/classFragment.java
+++ app/src/main/java/lanou/gift/sort/classFragment.java
@@ -1,9 +1,24 @@
 package lanou.gift.sort;
 
-import android.support.v4.app.Fragment;
+import lanou.gift.R;
+import lanou.gift.base.BaseFragment;
 
 /**
  * Created by dllo on 16/10/21.
  */
-public class classFragment extends Fragment {
+public class classFragment extends BaseFragment {
+    @Override
+    protected void initDate() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected int getLayout() {
+        return R.layout.class_fragment;
+    }
 }
